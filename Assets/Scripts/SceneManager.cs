0f4f174ae2e467e8e757bc11b2af5a82cfe8c688
TSUKASA MIYAKE <91399250+mi-TSUKASA@users.noreply.github.com>
2022-09-17 22:38:58 +0900

    特定の番号で魚がヒットする処理

--- Assets/Scripts/SceneManager.cs
+++ Assets/Scripts/SceneManager.cs
@@ -8,15 +8,60 @@ public class SceneManager : MonoBehaviour
     public GameObject rod;
     public AudioSource audioSourceSE;
     public AudioClip audioClipRod;
-    public bool throwRod = false;
+
+    public bool throwRod = false; //竿を投げたかどうか
+
+    //釣果
+    public GameObject hotate;
+    public GameObject fish;
+
     private void Update()
     {
+        //画面をタップした時
         if (Input.GetMouseButtonDown(0) && throwRod == false)
         {
-            rod.SetActive(true);
-            tapText.SetActive(false);
-            audioSourceSE.PlayOneShot(audioClipRod);
-            throwRod = true;
+            ThrowRod();
         }
+    }
+
+    void ThrowRod()
+    {
+        rod.SetActive(true);
+        tapText.SetActive(false);
+        audioSourceSE.PlayOneShot(audioClipRod);
+        throwRod = true;
+        HitFishes();
+    }
+
+    void HitFishes()
+    {
+        int hitFish = 0;
+        int hitHotate = 1;
+        int num = Random.Range(0, 100);
+        Debug.Log(num);
+        
+
+        while (num != hitFish && num != hitHotate)
+        {
+            num = Random.Range(0, 100);
+            Debug.Log(num);
+            if (num == hitFish)
+            {
+                Catch(hitFish);
+                break;
+            }
+            else if (num == hitHotate)
+            {
+                Catch(hitHotate);
+                break;
+            }
+        }
+        
+    }
+
+    void Catch(int hitNum)
+    {
+        List<GameObject> fishes = new List<GameObject>() { hotate, fish };
+        fishes[hitNum].SetActive(true);
     }
 }
