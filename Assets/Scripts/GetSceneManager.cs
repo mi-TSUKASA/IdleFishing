8645f4c861cf34f201fbaa703a00745004c560e1
TSUKASA MIYAKE <91399250+mi-TSUKASA@users.noreply.github.com>
2022-10-04 21:46:14 +0900

    釣れた魚を徐々に拡大するアクションを追加

--- Assets/Scripts/GetSceneManager.cs
+++ Assets/Scripts/GetSceneManager.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class GetSceneManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    GameObject prefab;
+    GameObject obj;
+
     void Start()
     {
         Catch(MainSceneManager.fishesId);
@@ -13,8 +15,17 @@ public class GetSceneManager : MonoBehaviour
 
     void Catch(int hitNum)
     {
-        GameObject prefab = (GameObject)Resources.Load("Prefabs/Catch/" + MainSceneManager.fishes[hitNum]);
-        Instantiate(prefab, new Vector3(0.0f, 2.0f, 0.0f), Quaternion.identity); //辞書からhitNumに対応したプレハブを生成する
+        prefab = (GameObject)Resources.Load("Prefabs/Catch/" + MainSceneManager.fishes[hitNum]);
+        obj = Instantiate(prefab, new Vector3(0.0f, 0.0f, 0.0f), Quaternion.identity); //辞書からhitNumに対応したプレハブを生成する
+        StartCoroutine("ScaleUp");
     }
 
+    IEnumerator ScaleUp()
+    {
+        for (float i = 0.1f; i < 0.45; i += 0.05f)
+        {
+            obj.transform.localScale = new Vector3(i, i, i);
+            yield return new WaitForSeconds(0.05f);
+        }
+    }
 }
